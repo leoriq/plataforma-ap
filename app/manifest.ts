fc3c67533d34bc63c1abfefbfb777f40c2858ae6
leonardo.salandim <leonardo.salandim@digitalsys.com.br>
2023-11-09 03:53:00 -0300

    fixes

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,13 +2,11 @@ import { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js App',
-    short_name: 'Next.js App',
-    description: 'Next.js App',
+    name: 'Abrindo Portas',
     start_url: '/',
     scope: '/',
     display: 'standalone',
-    background_color: '#FF0000',
-    theme_color: '#00FF00',
+    background_color: '#404266',
+    theme_color: '#404266',
   }
 }
